980222bd8b7f278df912ab9b009553a5365f02be
MahdiT54 <mahditanzim2@gmail.com>
2024-02-17 18:58:37 -0500

    movies

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=1c44ead
 // https://www.omdbapi.com/?s=fast&apikey=1c44ead
+
+const searchBtn = document.querySelector('.api-button');
+
+async function onSearchChange(event) {
+    const movieName = event.target.value;
+    localStorage.setItem('movieName', movieName);
+
+    searchBtn.innerHTML = `<i class="fa-solid fa-spinner fa-spin"></i>`;
+    searchBtn.classList.add("loading");
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    window.location.href = `${window.location.origin}/movie.html`;
+}
