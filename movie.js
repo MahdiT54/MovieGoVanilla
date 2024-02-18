52db4698bb523fdf989e506085b56bcaf713f7f3
MahdiT54 <mahditanzim2@gmail.com>
2024-02-18 04:20:55 -0500

    keyframe animations settled

---
+++ movie.js
@@ -0,0 +1,77 @@
+const movieListEl = document.querySelector(".movie__list");
+const userSearch = localStorage.getItem("movieName");
+const notFoundEl = document.querySelector(".not__found--wrapper");
+
+async function renderMovies(userSearch, filter) {
+  const movies = await fetch(
+    `https://www.omdbapi.com/?s=${userSearch}&apikey=1c44ead`
+  );
+  const moviesData = await movies.json();
+  const moviesSearch = moviesData.Search;
+
+  if (!moviesSearch || moviesSearch.length === 0) {
+    notFoundEl.innerHTML = `
+    <h1 class="not__found--error">'${userSearch}' was not found.</h1>
+    `;
+    movieListEl.innerHTML = "";
+    return;
+  }
+
+  if (filter === "NEW_TO_OLD") {
+    moviesSearch.sort((a, b) => {
+      const aYear = a.Year.includes("-")
+        ? parseInt(a.Year.split("-")[1])
+        : parseInt(a.Year);
+      const bYear = b.Year.includes("-")
+        ? parseInt(b.Year.split("-")[1])
+        : parseInt(b.Year);
+      return bYear - aYear;
+    });
+  } else if (filter === "OLD_TO_NEW") {
+    moviesSearch.sort((a, b) => {
+      const aYear = a.Year.includes("-")
+        ? parseInt(a.Year.split("-")[0])
+        : parseInt(a.Year);
+      const bYear = b.Year.includes("-")
+        ? parseInt(b.Year.split("-")[0])
+        : parseInt(b.Year);
+      return aYear - bYear;
+    });
+  }
+  showMovies = moviesSearch
+    .filter((movie, index) => index < 9)
+    .map((movie) => movieHTML(movie))
+    .join("");
+  movieListEl.innerHTML = showMovies;
+}
+
+function filterMovies(event) {
+  renderMovies(userSearch, event.target.value);
+}
+
+function getTitle(movieTitle) {
+  localStorage.setItem("movieTitle", movieTitle);
+  window.location.href = `${window.location.origin}/movieDetails.html`;
+}
+
+renderMovies(userSearch);
+console.log(userSearch);
+
+function movieHTML(movie) {
+  let poster = movie.Poster;
+  if (poster === "N/A") {
+    poster = "http://www.movienewz.com/img/films/poster-holder.jpg";
+  }
+  return `
+    <div class="movie" onclick="getTitle('${movie.imdbID}')">
+                <figure class="movie__img--wrapper">
+                    <img src='${poster}' alt="movie poster" class="movie__img">
+                </figure>
+                <div class="movie__description">
+                    <p class="movie__title">${movie.Title}</p>
+                    <p class="media__type">${movie.Type}</p>
+                    <p class="movie__year">${movie.Year}</p>
+                </div>
+            </div>
+    `;
+}
---
+++ movieDetails.js
@@ -0,0 +1,84 @@
+let movieTitle = localStorage.getItem("movieTitle"); // movie.imdbID from showMovies --> (movie) => movieHTML(movie)
+let movieName = localStorage.getItem("movieName"); // event.target.value from search__input from index.js
+
+const movieDetailsEl = document.querySelector(".movie__details");
+const movieListEl = document.querySelector(".movie__list");
+
+async function main(title) {
+  const movies = await fetch(
+    `https://www.omdbapi.com/?apikey=1c44ead&i=${title}`
+  );
+  const movieData = await movies.json();
+  movieDetailsEl.innerHTML = detailsHTML(movieData);
+}
+
+main(movieTitle);
+
+function detailsHTML(movie) {
+  return `
+    <div class="movie__details--title">${movie.Title}</div>
+            <div class="movie__details--movie-wrapper">
+              <figure class="movie__details--img-wrapper">
+                <img
+                  src="${movie.Poster}"
+                  alt="movie poster"
+                  class="movie__details--img"
+                />
+              </figure>
+              <div class="movie__details--description">
+                <p class="movie__description--p"><b class="cyan">Plot:</b> ${movie.Plot}</p>
+                <p class="movie__description--p"><b class="cyan">Type:</b> ${movie.Type}</p>
+                <p class="movie__description--p"><b class="cyan">Rated:</b> ${movie.Rated}</p>
+                <p class="movie__description--p"><b class="cyan">Genre:</b> ${movie.Genre}</p>
+                <p class="movie__description--p">
+                  <b class="cyan">Ratings:</b> ${movie.imdbRating}
+                </p>
+                <p class="movie__description--p"><b class="cyan">Votes:</b> ${movie.imdbVotes}</p>
+                <p class="movie__description--p">
+                  <b class="cyan">Release Date:</b> ${movie.Released}
+                </p>
+              </div>
+            </div>
+    `;
+}
+
+async function similarMovies(currentMovieID, movieName) {
+  const movies = await fetch(
+    `https://www.omdbapi.com/?s=${movieName}&apikey=1c44ead`
+  );
+  const moviesData = await movies.json();
+  const moviesSearch = moviesData.Search;
+  const limitedMovies = moviesSearch
+    .filter((movie) => movie.imdbID !== currentMovieID)
+    .slice(0, 3);
+  showMovies = limitedMovies.map((movie) => similarMoviesHTML(movie)).join("");
+  movieListEl.innerHTML = showMovies;
+}
+
+function similarMoviesHTML(movie) {
+  let poster = movie.Poster;
+  if (poster === "N/A") {
+    poster = "http://www.movienewz.com/img/films/poster-holder.jpg";
+  }
+  return `
+    <div class="movie" onclick="getTitle('${movie.imdbID}')">
+                <figure class="movie__img--wrapper">
+                    <img src='${poster}' alt="movie poster" class="movie__img">
+                </figure>
+                <div class="movie__description">
+                    <p class="movie__title">${movie.Title}</p>
+                    <p class="media__type">${movie.Type}</p>
+                    <p class="movie__year">${movie.Year}</p>
+                </div>
+            </div>
+    `;
+}
+
+function getTitle(title) {
+    localStorage.setItem("movieTitle", title);
+    window.location.href = "movieDetails.html"
+    console.log(title);
+}
+
+const currentMovieID = localStorage.getItem("movieTitle");
+similarMovies(currentMovieID, movieName);
