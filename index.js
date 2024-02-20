740286c9ce2e6d5c5adcd0a22539688b797af0fa
MahdiT54 <mahditanzim2@gmail.com>
2024-02-19 23:33:27 -0500

    responsiveness

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=1c44ead
 // https://www.omdbapi.com/?s=fast&apikey=1c44ead
 
-const searchBtn = document.querySelector('.api-button');
+const searchBtn = document.querySelector(".api-button");
 
 async function onSearchChange(event) {
-    const movieName = event.target.value;
-    localStorage.setItem('movieName', movieName);
+  const movieName = event.target.value;
+  localStorage.setItem("movieName", movieName);
 
-    searchBtn.innerHTML = `<i class="fa-solid fa-spinner fa-spin"></i>`;
-    searchBtn.classList.add("loading");
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    window.location.href = `${window.location.origin}/movie.html`;
+  searchBtn.innerHTML = `<i class="fa-solid fa-spinner fa-spin"></i>`;
+  searchBtn.classList.add("loading");
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  window.location.href = `${window.location.origin}/movie.html`;
+}
+
+function openMenu() {
+  document.body.classList += " menu--open";
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
 }
